5bd6a94be91b623d2d73444dd91012607b576bbc
Hamza <lelouch.hamza@gmail.com>
2019-01-24 17:09:57 +0000

    feat: Add functionality to sidebar (#5)
    * feat: Add hamburger icon to nav
    
    * feat: Add sidebar initial design
    
    * feat: Add sidebar colors
    
    * feat: Add font icons to sidebar
    
    * feat: Add brand logo
    
    * refactor: Set brand name class
    
    Giving the brand name 'h1' a class name to make it easier to select/modify
    
    * feat: Add sidebar 'shrunken' state
    
    * feat: Add sidebar toggle (animated)
    
    * refactor: Replace css hamburger with font icon

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-//console.log('\'Allo \'Allo!');
+class App {
+  constructor() {
+    this.sideMenuToggleButton = document.querySelector('.hamburger-js');
+    this.sideMenu = document.querySelector('.side-menu-js')
+  }
+
+  toggleSideMenu() {
+    this.sideMenuToggleButton.addEventListener('click', () => {
+      this.sideMenu.classList.toggle('shrunken');
+    });
+  }
+
+  init() {
+    this.toggleSideMenu();
+  }
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  const app = new App();
+  app.init();
+});
